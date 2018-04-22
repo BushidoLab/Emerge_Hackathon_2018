c1b8b57b5ccb7b943c73ad4587ad237f382d12c4
NL <nlevo@NLs-MacBook-Pro.local>
2018-04-22 02:15:37 -0400

    Fix detect

--- test.js
+++ test.js
@@ -5,24 +5,30 @@ Dotenv.config({ silent: true });
 
 const payload = {
 
-    image: 'https://qz.com/wp-content/uploads/2015/09/rtr4n4v3.jpg?quality=80&strip=all',
-    gallery_name: 'testGallery'
+    "image": 'https://qz.com/wp-content/uploads/2015/09/rtr4n4v3.jpg?quality=80&strip=all',
+    "gallery_name": 'testGallery8'
 };
 
 const { callKairos, recognize } = require('./lib/kairos');
-// const { uploadService, mapFaces } = require('./lib/cloudinary');
+const { uploadService, mapFaces } = require('./lib/cloudinary');
 
-// const x = fs.readFileSync('img.txt').toString();
+const x = fs.readFileSync('img.txt').toString();
 
-// (async function () {
+(async function () {
 
-//     const response = await uploadService(x);
-//     const faceURLs = mapFaces(response);
+    const response = await uploadService(x);
+    const faceURLs = mapFaces(response);
 
-//     console.log(faceURLs.map(async (image) => {
-//         console.log(image);
-//         return await callKairos(Object.assign({}, payload, { image }));
-//     }));
-// })();
+    const detected = await faceURLs.map(async (image) => {
+        console.log(image);
+        const response = await callKairos(Object.assign({}, payload, { image }));
 
-recognize(payload).then(console.log);
+        console.log(response);
+
+        return response
+    });
+
+
+    console.log(detected);
+})();
+

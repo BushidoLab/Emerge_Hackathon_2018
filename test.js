5b8613a704c516f26c28e42fdea11cde86e0039e
Danilo Alonso <damusix@gmail.com>
2018-04-21 22:24:55 -0400

    kairos

--- test.js
+++ test.js
@@ -3,13 +3,24 @@ const fs = require('fs');
 const Dotenv = require('dotenv');
 Dotenv.config({ silent: true });
 
+const payload = {
+
+    image: 'image',
+    gallery_name: 'testGallery'
+};
+
+const { callKairos } = require('./lib/kairos');
 const { uploadService, mapFaces } = require('./lib/cloudinary');
 
 const x = fs.readFileSync('img.txt').toString();
 
 (async function () {
 
-  const response = await uploadService(x);
+    const response = await uploadService(x);
+    const faceURLs = mapFaces(response);
 
-  console.log(mapFaces(response));
+    console.log(faceURLs.map(async (image) => {
+        console.log(image);
+        return await callKairos(Object.assign({}, payload, { image }));
+    }));
 })();
